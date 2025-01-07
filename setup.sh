fa3bd6cf4f69c87a28f0d25a2f9c32c0368a925c
incrig <kpalcins@gmail.com>
2025-01-07 15:40:32 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,38 +4,28 @@
 # echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/---USERNAME---/.zprofile
 # eval "$(/opt/homebrew/bin/brew shellenv)"
 
-
 brew update && brew upgrade
-
-
 brew install zsh kubectx kube-ps1 zsh-syntax-highlighting watch inetutils wget
-brew install --cask warp
 
-source "/opt/homebrew/opt/kube-ps1/share/kube-ps1.sh"
+# Install Hyper terminal & config
+brew install --cask hyper
+wget https://raw.githubusercontent.com/incrig/macsetup/refs/heads/main/config/hyper.js -o /Users/---USERNAME---/.hyper.js
 
 # Install OZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/Dbz/kube-aliases.git ~/.oh-my-zsh/custom/plugins/kube-aliases
-echo "plugins+=(kube-aliases)" >> ~/.zshrc
-wget https://raw.githubusercontent.com/incrig/macsetup/refs/heads/main/config/hyper.js -o /Users/---USERNAME---/.hyper.js
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k\n
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab\n
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions\n
+git clone https://github.com/Dbz/kube-aliases.git ~/.oh-my-zsh/custom/plugins/kube-aliases && echo "plugins+=(kube-aliases)" >> ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
-vi .zshrc
-
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime\n
+# Install VIM config
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 
 # Additional stuff
 # Make sure kube-ps1 is loaded from your ~/.zshrc and/or ~/.bashrc:
-#   source "/opt/homebrew/opt/kube-ps1/share/kube-ps1.sh"
-#   PS1='$(kube_ps1)'$PS1
-# To activate the syntax highlighting, add the following at the end of your .zshrc:
-#   source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+echo "/opt/homebrew/opt/kube-ps1/share/kube-ps1.sh" >> ~/.zshrc
+echo "PS1='$(kube_ps1)'$PS1" >> ~/.zshrc
 
 # If you receive "highlighters directory not found" error message,
 # you may need to add the following to your .zshenv:

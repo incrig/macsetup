e5ee94dac32f3b20ab43c2b406e81ba49f64ba3b
incrig <kpalcins@gmail.com>
2025-01-07 13:12:02 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@
 
 
 brew update && brew upgrade
-brew install ctx shasum kube-ps1 zsh-syntax-highlighting watch ftp inetutils
+brew install kubectx shasum kube-ps1 zsh-syntax-highlighting watch ftp inetutils
 source "/opt/homebrew/opt/kube-ps1/share/kube-ps1.sh"
 echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc

5c29e7727e57a38c41a41dc9bdba780a43821efd
incrig <kpalcins@gmail.com>
2025-01-07 16:25:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install basic packages
-brew install zsh kubectx kube-ps1 zsh-syntax-highlighting watch inetutils wget helm 
+brew install zsh kubectx kube-ps1 zsh-syntax-highlighting watch inetutils wget helm k9s awscli
 
 # Install Hyper terminal & config
 brew install --cask hyper
@@ -15,13 +15,12 @@ wget https://raw.githubusercontent.com/incrig/macsetup/refs/heads/main/config/.h
 # Install OZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/Dbz/kube-aliases.git ~/.oh-my-zsh/custom/plugins/kube-aliases && echo "plugins+=(kube-aliases)" >> ~/.zshrc
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 # Install ZSH theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+sed -i '' 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' /Users/$(whoami)/.zshrc
 
 # Install VIM config
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
@@ -33,7 +32,20 @@ chmod +x "/opt/homebrew/opt/kube-ps1/share/kube-ps1.sh"
 source "/opt/homebrew/opt/kube-ps1/share/kube-ps1.sh"
 echo "/opt/homebrew/opt/kube-ps1/share/kube-ps1.sh" >> ~/.zshrc
 
-# Configure .zshrc
+# Configure .zshrc plugins
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+echo "plugins+=(fzf-tab)" >> ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "plugins+=(zsh-autosuggestions)" >> ~/.zshrc
+
+echo "plugins+=(aws)" >> ~/.zshrc
+echo "plugins+=(terraform)" >> ~/.zshrc
+echo "plugins+=(vscode)" >> ~/.zshrc
+echo "plugins+=(kubectl)" >> ~/.zshrc
+echo "plugins+=(Kube-ps1)" >> ~/.zshrc
+
+
 
 # Start hyper terminal. Should be configured at this point
 hyper

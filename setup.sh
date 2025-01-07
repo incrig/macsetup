660227b9509de8a16fa8ce437a6519579a21e48a
incrig <kpalcins@gmail.com>
2025-01-07 13:09:00 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Add command to post-setup brew eval commands
+
 brew update && brew upgrade
 brew install ctx shasum kube-ps1 zsh-syntax-highlighting watch ftp inetutils
 source "/opt/homebrew/opt/kube-ps1/share/kube-ps1.sh"

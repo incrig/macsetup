74d160a93f14e115f799e46e79753be453f6390e
incrig <kpalcins@gmail.com>
2025-01-07 13:10:54 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # Add command to post-setup brew eval commands
+# echo >> /Users/---USERNAME---/.zprofile
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/---USERNAME---/.zprofile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+
 
 brew update && brew upgrade
 brew install ctx shasum kube-ps1 zsh-syntax-highlighting watch ftp inetutils
